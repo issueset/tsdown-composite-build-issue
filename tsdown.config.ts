c7a2f04a686ccb920b5ff8263c0994a0209717d6
ocavue <ocavue@gmail.com>
2025-06-01 16:06:26 +1000

    simplify

--- tsdown.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "tsdown";
-
-export default defineConfig({
-  entry: ["src/dom/dom.ts", "src/node/node.ts"],
-  outDir: "dist/dist_tsdown",
-  format: "esm",
-  dts: true,
-});
